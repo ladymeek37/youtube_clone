ba29e215a20d2c4ea2f5a8da872981ad3e464aec
Lady Meek <ladyjmeek@gmail.com>
2023-02-25 11:12:55 -0800

    added view function to get all comments and protected function to add comments

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+
+# responds with all comments from the database that are related to the video id sent in the URL
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def video_comments(request):
+    comments = Comment.objects.filter(video_id = request.video_id)
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes({IsAuthenticated})
+def add_comment(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
